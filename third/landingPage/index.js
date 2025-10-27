faa86d42d486acf351cad5445e4155fee3d32884
ramzan617 <asiframzan2021@gmail.com>
2025-10-27 21:59:52 +0500

    landing page enhanced

--- third/landingPage/index.js
+++ third/landingPage/index.js
@@ -1,16 +1,18 @@
+//get current year for footer section
 document.getElementById("year").textContent = new Date().getFullYear();
 
-let darkmode = localStorage.getItem('darkmode');
+// darkmode and lightmode toggling
 const themeSwitch = document.getElementById('theme_switch');
+let darkmode = localStorage.getItem('darkmode');
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode');
+  document.documentElement.classList.add('darkmode');
   localStorage.setItem('darkmode', 'active');
 };
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode');
-  localStorage.setItem('darkmode', 'inactive'); 
+  document.documentElement.classList.remove('darkmode');
+  localStorage.setItem('darkmode', 'inactive');
 };
 
 if (darkmode === 'active') {
@@ -23,3 +25,14 @@ themeSwitch.addEventListener('click', () => {
   darkmode = localStorage.getItem('darkmode');
   darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
 });
+
+
+// mobile menu responsiveness
+const menuOpen = document.getElementById("menu_open");
+const menuClose = document.getElementById("menu_close");
+const mobile_menu = document.querySelector(".mobile_menu_svg");
+const navMenu = document.getElementById("nav_menu");
+mobile_menu.addEventListener("click", () => {
+  navMenu.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
+})
