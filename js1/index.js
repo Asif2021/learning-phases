4102f4645ab69a8c7dddc486e3a14e63ef2862be
ramzan617 <asiframzan2021@gmail.com>
2025-11-20 22:39:12 +0500

    js first assignment added

---
+++ js1/index.js
@@ -0,0 +1,28 @@
+const user = prompt("please enter your name");
+alert("Hello "+ user);
+console.log(user);
+
+const myName = 'Asif';
+let myAge = 30;
+const isStudent = true;
+console.log("My Name is " + myName);
+console.log("My age is " + myAge);
+console.log("Am I Student? " + isStudent);
+
+let subtract = 10 - 5;
+let multiply = 10 * 5;
+if(multiply > subtract){
+    console.log("hello world")
+}
+const isEqual = subtract === multiply;
+console.log(isEqual);
+
+const remainder = subtract % multiply;
+console.log(remainder);
+
+
+
+
+
+
+
