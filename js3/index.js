9f948f345c2e6502a989660ed99c9db9ec45f9fa
ramzan617 <asiframzan2021@gmail.com>
2025-11-29 00:48:22 +0500

    js3 added

--- js3/index.js
+++ js3/index.js
@@ -22,24 +22,53 @@ function reset(){
 
 
 
-function show(){
-    document.getElementsByClassName("text")[0].style.visibility = 'visible';
+function showHide(){
+    let showHide = document.getElementById('show_hide');
+    showHide.classList.toggle('text');
 }
-function hide(){
-    document.getElementsByClassName("text")[0].style.visibility = "hidden";
-}
+let btnText= document.getElementById('showHide_button');
+btnText.addEventListener('click', function(){
+    if(btnText.innerText === 'show'){
+        btnText.innerText = 'hide';
+    } else {
+        btnText.innerText = 'show';
+    }
+})
 
 
 
+   
 function addTodo(){
-    const input = document.getElementById('input').value;
-    if(input === '') return;
-    const todos = document.createElement('p');
-    todos.style.textAlign = 'center';
-    todos.style.fontSize = '30px';
-    todos.innerText = input;
-    document.getElementById('all_todos').appendChild(todos);
-    document.getElementById('input').value = '' 
+    const input = document.getElementById('input');
+    const allTodos = document.getElementById('all_todos');
+    if(input.value === '') return;
+    const li = document.createElement('li');
+    li.innerHTML = input.value;
+    allTodos.appendChild(li);
+    input.value = '';
+    const span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    li.appendChild(span);
+     saveTodos();
+   }
+
+const allTodos = document.getElementById('all_todos');
+    allTodos.addEventListener('click', function(e){
+        if(e.target.tagName === 'LI'){
+            e.target.classList.toggle('checked');
+            saveTodos();
+        } else if (e.target.tagName === 'SPAN') {
+            e.target.parentElement.remove();
+            saveTodos();
+            }
+    });
+
+function saveTodos(){
+    localStorage.setItem('todos', allTodos.innerHTML);
 }
 
+function showTodos(){
+    allTodos.innerHTML = localStorage.getItem('todos');
+}
+showTodos();
 
